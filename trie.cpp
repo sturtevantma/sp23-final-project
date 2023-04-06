018ac68a9ad1272cd8a6885e8c7fb0c05e1f2d02
hcz4 <hcz4th@gmail.com>
2023-04-06 16:21:51 -0400

    contains method

--- trie.cpp
+++ trie.cpp
@@ -16,6 +16,29 @@ void Trie::dictionary(std::ostream &os, std::string delim) {
     this->dictionary(this->root, os, delim);
 }
 
+bool Trie::contains(std::string s) {
+
+    TrieNode* temp = this->root;
+    //Loop over string 
+    for (int i = 0; i < s.length(); i++) {
+        //If the node has the child
+        if (temp->children[s.at(i)]) {
+            //Assign temp to the child and continue to next iteration
+            temp = temp->children[s.at(i)];
+            continue;
+        }
+        //Else if the child is never found then return false
+        else if (!temp->children[s.at(i)]) {
+            return false;
+        }
+    }
+    //If we looped over the entire word then we check if the node is an endpoint to signal a valid word
+    if (temp->endpoint) {
+        return true;
+    }
+    return false;
+}
+
 void Trie::dictionary(TrieNode* root, std::ostream &os, std::string delim) {
     if(root == nullptr) {
         return;
